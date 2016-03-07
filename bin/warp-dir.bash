f38dcd5732f7494c8f11ca3a29a9489543d32f38
Konstantin Gredeskoul <kig@reinvent.one>
2016-03-06 23:39:37 -0800

    Better handling of errors and installation hints

--- bin/warp-dir.bash
+++ bin/warp-dir.bash
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-# warp-dir auto installer
-
-wd() {
-  executable=warp-dir
-  which warp-dir 2>&1 > /dev/null
-  have_executable=0
+function wd {
+  $(which 'warp-dir') 2>&1 > /dev/null
+  have_executable=$?
   if [ ${have_executable} -eq 0 ]; then
-    ifs_backup=$IFS
+    IFS_BACKUP=$IFS
     IFS="+"
     output=$(warp-dir $@ 2>&1)
     code=$?
     eval ${output}
-    IFS=$ifc_backup
+    IFS=$IFS_BACKUP
   else
-    echo "Can't find warp-dir executable!"
+    printf "\nWhoops – I can't find 'warp-dir' executable.\n"
+    printf "Is the gem properly installed? Sometimes it helps to run 'hash -r'.\n"
+    printf "\nTry reinstalling the gem with:\n\n"
+    printf "    $ gem install warp-dir --no-wrappers\n"
+    printf "    $ hash -r\n"
+    printf "    $ warp-dir install\n"
   fi
 }
-
-
