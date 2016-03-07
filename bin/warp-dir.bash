e5caf1c6e6e643edaff5300f1b5270f0503bfc68
Konstantin Gredeskoul <kig@reinvent.one>
2016-03-06 23:23:44 -0800

    Make sure warp-dir is in the path or give error

--- bin/warp-dir.bash
+++ bin/warp-dir.bash
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
+# warp-dir auto installer
+
 wd() {
-  ifs_backup=$IFS
-  IFS="+" # this is needed so that the tabulated output does not collapse
-  output=$(warp-dir $@ 2>&1)
-  code=$?
-  eval ${output}
-  IFS=$ifc_backup
+  executable=warp-dir
+  which warp-dir 2>&1 > /dev/null
+  have_executable=0
+  if [ ${have_executable} -eq 0 ]; then
+    ifs_backup=$IFS
+    IFS="+"
+    output=$(warp-dir $@ 2>&1)
+    code=$?
+    eval ${output}
+    IFS=$ifc_backup
+  else
+    echo "Can't find warp-dir executable!"
+  fi
 }
+
+
