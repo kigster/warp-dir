eb63ab554c5e9bd74d9135c77f62c971e29a79cb
Konstantin Gredeskoul <kig@reinvent.one>
2016-03-07 17:08:57 -0800

    cd into a real local folder, if warp point not found

--- lib/warp/dir/command/warp.rb
+++ lib/warp/dir/command/warp.rb
@@ -9,13 +9,22 @@ module Warp
         needs_a_point? true
 
         def run(*args)
+          warp_to_path = nil
           if point.nil? && point_name
-            self.point = store[point_name]
+            begin
+              self.point = store[point_name]
+            rescue ::Warp::Dir::Errors::PointNotFound
+            end
           end
-          raise ::Warp::Dir::Errors::PointNotFound.new(point) unless point
-          p = point
+          if point
+            warp_to_path = point.absolute_path
+          else
+            files         = ::Dir.glob("#{point_name}*")
+            warp_to_path = files.first if files.size == 1
+          end
+          raise ::Warp::Dir::Errors::PointNotFound.new(point_name) unless warp_to_path
           on :shell do
-            message "cd #{p.absolute_path}"
+            message "cd #{warp_to_path}"
           end
         end
       end
--- lib/warp/dir/version.rb
+++ lib/warp/dir/version.rb
@@ -1,6 +1,6 @@
 module Warp
   module Dir
-    VERSION = '1.1.1'
+    VERSION = '1.1.2'
   end
 end
 
