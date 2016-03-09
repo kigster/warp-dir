f890e1aa2042c58405385c8180d51d3d8b22b300
Konstantin Gredeskoul <kig@wanelo.com>
2016-03-08 18:57:47 -0800

    More resilient shell function
     - Keep re-checking if warp-dir was not found last time
     - recognize 127 exit code (not found)

--- bin/warp-dir.bash
+++ bin/warp-dir.bash
@@ -2,7 +2,7 @@
 
 # warp-dir shell wrapper
 wd() {
-  if [ -z "${warp_dir_exec_installed}" ]; then
+  if [ -z "${warp_dir_exec_installed}" -o "${warp_dir_exec_installed}" == "1" ]; then
     $(which 'warp-dir') 2>&1 > /dev/null
     export warp_dir_exec_installed=$?
   fi
@@ -12,14 +12,23 @@ wd() {
     IFS="+"
     output=$(WARP_DIR_SHELL=yes warp-dir $@ 2>&1)
     code=$?
-    eval ${output}
-    IFS=$IFS_BACKUP
+    if [ $code -eq 127 ]; then
+      unset warp_dir_exec_installed
+      wd_not_found
+    else
+      eval ${output}
+      IFS=$IFS_BACKUP
+    fi
   else
+    wd_not_found
+  fi
+}
+
+wd_not_found() {
     printf "\nWhoops – I can't find 'warp-dir' executable.\n"
     printf "Is the gem properly installed?\n"
     printf "\nTry reinstalling the gem with, for example:\n\n"
     printf "    $ gem install warp-dir --no-wrappers\n"
     printf "    $ hash -r\n"
     printf "    $ warp-dir install [ --dotfile ~/.bashrc ]\n"
-  fi
 }
